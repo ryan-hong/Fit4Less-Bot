f0cf086f2df10b51a01e548fa4bd6891baa9e9ab
Ryan Hong <65246381+ryan-hong@users.noreply.github.com>
2021-03-10 21:29:47 -0400

    first commit, simple auto login feature

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Fit4Less-Bot
+
 Bot using python and selenium to auto-book workout time slots
+
+Install selenium module
+
+Install chromedriver, make sure it is in same directory as python script
+
+Have fun
---
+++ login.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# open login page for Fit4Less
+browser = webdriver.Chrome()
+browser.get(('https://myfit4less.gymmanager.com/portal/login.asp'))
+
+# username and password --> change to your credentials
+usernameStr = 'your email here'
+passwordStr = 'your password here'
+
+username = browser.find_element_by_id('emailaddress')
+username.send_keys(usernameStr)
+
+password = browser.find_element_by_id('password')
+password.send_keys(passwordStr)
+
+loginButton = browser.find_element_by_id('loginButton')
+loginButton.click()
